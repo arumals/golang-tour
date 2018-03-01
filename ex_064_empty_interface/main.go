5b6147ee9c1939288c24961e2fc9ac984c76fab5
arumals <m4ltos@gmail.com>
2018-02-28 19:30:15 -0500

    Completed ex_064_empty_interface

---
+++ ex_064_empty_interface/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func describe(i interface{}) {
+	fmt.Printf("%v %T\n", i, i)
+}
+
+func main() {
+	var i interface{}
+	describe(i)
+	i = 42
+	describe(i)
+	i = "Hello World"
+	describe(i)
+}
